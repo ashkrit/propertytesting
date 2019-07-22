6a361038fb9193baec24b3413d7d7e3d80795034
Ashkrit <ashkrit@gmail.com>
2019-07-22 08:47:48 +0800

    Add more stateful system

---
+++ src/test/scala/gen/Generators.scala
@@ -0,0 +1,23 @@
+package gen
+
+import org.scalacheck.Gen
+
+object Generators {
+
+  def main(args: Array[String]): Unit = {
+
+    val i = Gen.choose(0, 10000)
+    for (times <- 0 to 5) {
+      println(times + "->" + i.sample)
+    }
+
+
+    val pairOfNumbers = for {
+      x <- Gen.choose(1, 6)
+      y <- Gen.choose(1, 6)
+    } yield (x, y)
+
+
+    println(pairOfNumbers.sample)
+  }
+}
---
+++ src/test/scala/satefull/CounterApp.scala
@@ -0,0 +1,90 @@
+package satefull
+
+import org.scalacheck.{Gen, Prop}
+import org.scalacheck.commands._
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Gen.oneOf
+
+import scala.util.{Success, Try}
+
+object CounterApp {
+
+  case class Counter(private var n: Int = 0) {
+
+    def increment(): Int = {
+      if (n < Int.MaxValue) {
+        n = n + 1
+        n
+      } else {
+        throw new RuntimeException("Overflow")
+      }
+    }
+
+    def get: Int = n
+  }
+
+
+  object CounterCommands extends Commands {
+
+    type Sut = Counter
+    type State = Long
+
+    def newSut(state: State) = new Counter(state.toInt)
+
+    def genInitialState: Gen[State] = arbitrary[Int].map(_.toLong)
+
+    def genCommand(state: State): Gen[Command] = oneOf(Increment, Get)
+
+    case object Increment extends Command {
+      type Result = Int
+
+      def run(counter: Sut) = counter.increment()
+
+      def nextState(s: State) = if (s < Int.MaxValue) s + 1 else s
+
+      def preCondition(s: State) = s < Int.MaxValue
+
+      def postCondition(s: State, result: Try[Int]) =
+        if (s >= Int.MaxValue) result.isFailure
+        else
+          result == Success(s + 1)
+    }
+
+    case object Get extends Command {
+      type Result = Int
+
+      def run(counter: Sut) = counter.get
+
+      def nextState(s: State) = s
+
+      def preCondition(s: State) = true
+
+      def postCondition(s: State, result: Try[Int]) =
+        result == Success(s)
+
+
+    }
+
+    override def canCreateNewSut(newState: CounterCommands.State,
+                                 initSuts: Traversable[CounterCommands.State],
+                                 runningSuts: Traversable[CounterCommands.Sut]): Boolean = true
+
+
+    override def destroySut(sut: CounterCommands.Sut): Unit = {}
+
+    override def initialPreCondition(state: CounterCommands.State): Boolean = true
+
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val counterProp: Prop = CounterCommands.property()
+    counterProp.check()
+
+  }
+
+
+}
+
+
+
